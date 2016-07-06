77dbb1f7e7405581409bce5e921e9c6285d46c16
marian.bejenari <marian.bejenari@MDDSK40096>
2016-07-06 10:56:37 +0300

    Add project files.

---
+++ src/SharpLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpLinq
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
